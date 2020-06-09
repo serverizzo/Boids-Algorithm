3a178983a386fc3a5ecf370ded8be304fad3b72d
Edward Cerverizzo <edward.cerverizzo@gmail.com>
2020-06-09 12:26:50 -0400

    finished writeup and style is fine

--- sketch.js
+++ sketch.js
@@ -25,7 +25,7 @@ function setup() {
   Vision.mousePressed(toggleVision)
 
   prox = false;
-  Prox = createButton("prox")
+  Prox = createButton("Proximity")
   Prox.position(210, 15)
   Prox.mousePressed(toggleSeperation);
 
@@ -46,8 +46,23 @@ function setup() {
 
   // Write up
   createElement("h1", "Boids Algorithm")
-  createP("Boids (short for bird-iod object) algorithm was developed by Craig Reynolds in 1986 to simulate group movements e.g. bird flight, schools of fish. Though the simulation may seem complex, Boids algorithm works on three simple rules.")
+  createP("Boids (short for bird-iod object) algorithm was developed by Craig Reynolds in 1986 to simulate group movements e.g. bird flight, schools of fish. Though the simulation may seem complex, Boids algorithm works on three simple rules:")
+  createElement("li", "* Alignment: Fly with the pack")
+  createElement("li", "* Cohesion: Gravatate twords the center of the pack")
+  createElement("li", "* Seperation: Keep a distance from another boid")
 
+  createElement('h2', "Boid Vision")
+  createP("It is unreasonable for a single boid to consider all other boids when flying--in nature a bird or a fish would not see every other of its pack, just the ones closest to it. ")
+  createElement("li", "* Toggling the Alignment/Cohesion button shows what other boids a boid considers when applying alignment and cohesion to its flight path.")
+  createElement("li", "* Toggling the Proximity button will show the distance between two boids before then begin to be influenced by seperation.")
+  createElement("li", "* Overlapping circles trigger influencing vectors")
+
+  createElement('h2', "Motion")
+  createP("In simulation, the path directing the boid is governed by vector arithmetic. All rules produce a vector, the governing vector is the subtraction of all vectors i.e. result = -AlignmentVector - CohesionVector - SeperationVector.")
+  createElement("li", "* These Values can be manipulted by using the corresponding sliders")
+
+  createElement('h2', "More info")
+  createA("https://www.red3d.com/cwr/boids/", "For more information, check out Craig Reynolds website")
 
 }
 
